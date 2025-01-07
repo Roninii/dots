e6ca818e70dc7e89eea50c7a8249802ddcc98c03
Roninii <ronnie@ronini.dev>
2025-01-07 14:35:18 -0600

    2025 refresh

---
+++ nvim/lua/custom/plugins/tokyonight.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    enabled = true,
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd 'colorscheme tokyonight-night'
+    end,
+  },
+}
