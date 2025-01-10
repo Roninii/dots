c07a16fcccaf11693a52ebd816aab270f405c755
Roninii <ronnie@ronini.dev>
2025-01-10 15:51:53 -0600

    change theme

---
+++ nvim/lua/custom/plugins/everforest.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    enabled = true,
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = 'hard'
+      vim.g.everforest_enable_italic = true
+      vim.cmd 'colorscheme everforest'
+    end,
+  },
+}
