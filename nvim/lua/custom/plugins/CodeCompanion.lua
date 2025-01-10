e30c7919ff86d79e9678d6ebea32a0926f7be156
Roninii <ronnie@ronini.dev>
2025-01-10 16:52:32 -0600

    refactor codecompanion file

--- nvim/lua/custom/plugins/CodeCompanion.lua
+++ nvim/lua/custom/plugins/CodeCompanion.lua
@@ -4,25 +4,25 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
   },
-  config = function()
-    require('codecompanion').setup {
-      strategies = {
-        chat = { adapter = 'copilot' },
-        inline = {
-          adapter = 'copilot',
-        },
+  opts = {
+    strategies = {
+      chat = { adapter = 'copilot' },
+      inline = {
+        adapter = 'copilot',
       },
-      display = {
-        action_palette = {
-          provider = 'telescope',
-        },
+    },
+    display = {
+      action_palette = {
+        provider = 'telescope',
       },
-    }
+    },
+  },
+  init = function()
+    for _, mode in ipairs { 'n', 'v' } do
+      vim.api.nvim_set_keymap(mode, '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap(mode, '<LocalLeader>a', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true, desc = 'Open CodeCompanion Chat' })
+    end
 
-    vim.api.nvim_set_keymap('n', '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('v', '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<LocalLeader>a', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true, desc = 'Open CodeCompanion Chat' })
-    vim.api.nvim_set_keymap('v', '<LocalLeader>a', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true, desc = 'Open CodeCompanion Chat' })
     vim.api.nvim_set_keymap('v', 'ga', '<cmd>CodeCompanionChat Add<cr>', { noremap = true, silent = true })
 
     -- Expand 'cc' into 'CodeCompanion' in the command line
