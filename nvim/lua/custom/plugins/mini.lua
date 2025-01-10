a5fb1d9149f0f5ef573a0c21e21f8dd8a551ad05
Roninii <ronnie@ronini.dev>
2025-01-10 15:52:09 -0600

    organize mini plugin better

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -3,91 +3,8 @@ return {
     'echasnovski/mini.nvim',
     version = '*', -- Use the latest stable version
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
-
       require('mini.animate').setup()
-
-      require('mini.git').setup()
-      require('mini.diff').setup()
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      local statusline = require 'mini.statusline'
-      statusline.setup {}
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
-      -- Automatic highlighting of word under cursor
-      require('mini.cursorword').setup()
-
-      -- Highlight indent scope
-      require('mini.indentscope').setup()
-
-      -- Jump to next/previous single character
-      require('mini.jump').setup()
-
-      -- Show notifications
-      require('mini.notify').setup()
-
-      -- Move lines and selections in direction
-      require('mini.move').setup {
-        mappings = {
-          -- Visual mode
-          left = '<M-Left>',
-          right = '<M-Right>',
-          down = '<M-Down>',
-          up = '<M-Up>',
-          -- Normal mode
-          line_left = '<M-Left>',
-          line_right = '<M-Right>',
-          line_down = '<M-Down>',
-          line_up = '<M-Up>',
-        },
-      }
-
-      -- Start page
-      require('mini.starter').setup {
-        header = [[
-██████╗  ██████╗ ███╗   ██╗██╗███╗   ██╗██╗
-██╔══██╗██╔═══██╗████╗  ██║██║████╗  ██║██║
-██████╔╝██║   ██║██╔██╗ ██║██║██╔██╗ ██║██║
-██╔══██╗██║   ██║██║╚██╗██║██║██║╚██╗██║██║
-██║  ██║╚██████╔╝██║ ╚████║██║██║ ╚████║██║
-╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝╚═╝
-        ]],
-        footer = '',
-      }
-
-      -- File utils
-      require('mini.files').setup {
-        windows = {
-          preview = true,
-          width_preview = 100,
-        },
-      }
-      local minifiles_toggle = function()
-        if not MiniFiles.close() then
-          MiniFiles.open()
-        end
-      end
-      vim.keymap.set('n', '<leader>ft', minifiles_toggle, { desc = '[F]ile [T]ree' })
-
-      -- Show keybindings on the fly
       local miniclue = require 'mini.clue'
       miniclue.setup {
         window = {
@@ -139,9 +56,65 @@ return {
           miniclue.gen_clues.z(),
         },
       }
-
-      -- Pair matching
+      require('mini.cursorword').setup()
+      require('mini.diff').setup()
+      require('mini.files').setup {
+        windows = {
+          preview = true,
+          width_preview = 100,
+        },
+      }
+      require('mini.git').setup()
+      require('mini.icons').setup()
+      require('mini.indentscope').setup()
+      require('mini.jump').setup()
+      require('mini.move').setup {
+        mappings = {
+          -- Visual mode
+          left = '<M-Left>',
+          right = '<M-Right>',
+          down = '<M-Down>',
+          up = '<M-Up>',
+          -- Normal mode
+          line_left = '<M-Left>',
+          line_right = '<M-Right>',
+          line_down = '<M-Down>',
+          line_up = '<M-Up>',
+        },
+      }
+      require('mini.notify').setup()
       require('mini.pairs').setup()
+      local statusline = require 'mini.statusline'
+      require('mini.starter').setup {
+        header = [[
+██████╗  ██████╗ ███╗   ██╗██╗███╗   ██╗██╗
+██╔══██╗██╔═══██╗████╗  ██║██║████╗  ██║██║
+██████╔╝██║   ██║██╔██╗ ██║██║██╔██╗ ██║██║
+██╔══██╗██║   ██║██║╚██╗██║██║██║╚██╗██║██║
+██║  ██║╚██████╔╝██║ ╚████║██║██║ ╚████║██║
+╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝╚═╝
+        ]],
+        footer = '',
+      }
+      require('mini.surround').setup()
+
+      -- Mock Neovim web devicons globally so that Telescope calls mini.icons
+      MiniIcons.mock_nvim_web_devicons()
+
+      -- Modify statusline to show line and column number with : instead of |
+      statusline.setup {}
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+
+      -- Add a function and keymap for toggling the file tree
+      local minifiles_toggle = function()
+        if not MiniFiles.close() then
+          MiniFiles.open()
+        end
+      end
+      vim.keymap.set('n', '<leader>ft', minifiles_toggle, { desc = '[F]ile [T]ree' })
     end,
   },
 }
