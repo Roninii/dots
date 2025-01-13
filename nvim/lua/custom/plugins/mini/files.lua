166a27c8f26bfd660f40f7a2d19495468aa745aa
Roninii <ronnie@ronini.dev>
2025-01-13 13:58:52 -0600

    add fn for opening file in a split

--- nvim/lua/custom/plugins/mini/files.lua
+++ nvim/lua/custom/plugins/mini/files.lua
@@ -11,6 +11,45 @@ return {
     },
   },
   init = function()
+    local map_split = function(buf_id, lhs, direction)
+      local rhs = function()
+        -- Make new window and set it as target
+        local cur_target = MiniFiles.get_explorer_state().target_window
+        if not vim.api.nvim_win_is_valid(cur_target) then
+          print 'Current target window is invalid'
+          return
+        end
+
+        local new_target = vim.api.nvim_win_call(cur_target, function()
+          vim.cmd(direction .. ' split')
+
+          ---@diagnostic disable-next-line: redundant-return-value
+          return vim.api.nvim_get_current_win()
+        end)
+
+        if not vim.api.nvim_win_is_valid(new_target) then
+          print 'New target window is invalid'
+          return
+        end
+
+        MiniFiles.set_target_window(new_target)
+      end
+
+      -- Adding `desc` will result into `show_help` entries
+      local desc = 'Split ' .. direction
+      vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        -- Tweak keys to your liking
+        map_split(buf_id, '<C-s>', 'belowright horizontal')
+        map_split(buf_id, '<C-v>', 'belowright vertical')
+      end,
+    })
+
     -- Add a function and keymap for toggling the file tree
     local minifiles_toggle = function()
       if not MiniFiles.close() then
