dfd14340729c2a617fa727b2187acb92749d1eb2
Ronnie Villarini <ronnie@ronini.dev>
2025-02-25 14:55:36 -0600

    add root configs (zsh stuff)

--- create_sym_links.sh
+++ create_sym_links.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-# Define an array of directory/file names
-targets=(
+# Define an array of directory/file names for .configs
+d_config_targets=(
     "btop"
     "ghostty"
     "github-copilot"
@@ -13,9 +13,21 @@ targets=(
     "spaceship.zsh"
     "spaceship.zsh.zwc"
     "zed"
+    "nvim"
 )
 
-# Loop through the array and run the ln command for each target
-for target in "${targets[@]}"; do
-  ln -s ~/dots/"$target" "$target"
+# Define an array of directory/file names for root configs
+root_config_targets=(
+  ".zsh_history"
+  ".zshrc"
+)
+
+# Loop through the array and run the ln command for each .config target
+for target in "${d_config_targets[@]}"; do
+  ln -s ~/dots/"$target" ~/.config/"$target"
+done
+
+# Loop through the array and run the ln command for each root target
+for target in "${root_config_targets[@]}"; do
+  ln -s ~/dots/"$target" ~/"$target"
 done
