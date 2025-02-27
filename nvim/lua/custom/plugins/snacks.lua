b1786d93503c7bbf4131021a260a64e180c7b54c
Ronnie Villarini <ronnie@ronini.dev>
2025-02-27 09:41:13 -0600

    add keybind for yanking git permalink to clipboard

--- nvim/lua/custom/plugins/snacks.lua
+++ nvim/lua/custom/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
     opts = {
       zen = { enabled = true },
       lazygit = { enabled = true },
+      gitbrowse = { enabled = true },
     },
     keys = {
       {
@@ -21,6 +22,23 @@ return {
           Snacks.lazygit()
         end,
         desc = 'Lazy[G]it',
+      },
+      {
+        '<leader>gy',
+        function()
+          Snacks.gitbrowse.open {
+            open = function(url)
+              local id = MiniNotify.add('Yanked url to clipboard', 'INFO')
+              vim.defer_fn(function()
+                MiniNotify.remove(id)
+              end, 3000)
+
+              vim.fn.setreg('+', url)
+            end,
+          }
+        end,
+        desc = '[Y]ank Git URL',
+        mode = { 'n', 'v' },
       },
     },
   },
