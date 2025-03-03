e3e49084324fd68066d22cec3523c842b627d32d
Ronnie Villarini <ronnie@ronini.dev>
2025-03-03 11:56:32 -0600

    replace copilot.vim with copilot.lua

--- nvim/lua/custom/plugins/copilot.lua
+++ nvim/lua/custom/plugins/copilot.lua
@@ -1,5 +1,10 @@
 return {
   {
-    'github/copilot.vim',
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {}
+    end,
   },
 }
