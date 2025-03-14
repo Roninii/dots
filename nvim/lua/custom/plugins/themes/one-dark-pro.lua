a2b228136559fda7484b19b336e0007ba6ba2e57
Ronnie Villarini <ronnie@ronini.dev>
2025-03-14 11:56:03 -0500

    one dark defaults even though I don't use it lol

--- nvim/lua/custom/plugins/themes/one-dark-pro.lua
+++ nvim/lua/custom/plugins/themes/one-dark-pro.lua
@@ -5,8 +5,18 @@ return {
   priority = 1000,
   opts = {
     styles = {
-      types = 'italic',
+      types = 'NONE',
+      methods = 'NONE',
+      numbers = 'NONE',
+      strings = 'NONE',
       comments = 'italic',
+      keywords = 'bold,italic',
+      constants = 'NONE',
+      functions = 'italic',
+      operators = 'NONE',
+      variables = 'NONE',
+      parameters = 'NONE',
+      conditionals = 'italic',
       virtual_text = 'italic',
     },
     options = {
