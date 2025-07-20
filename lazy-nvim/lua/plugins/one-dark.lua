7554daa19d8ccef5080872c70cf8ade4032c82b3
Ronnie Villarini <ronnie@ronini.dev>
2025-07-20 18:20:12 -0500

    add Anthropic API key for codecompanion config

---
+++ lazy-nvim/lua/plugins/one-dark.lua
@@ -0,0 +1,27 @@
+return {
+  enabled = true,
+  "olimorris/onedarkpro.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    styles = {
+      types = "NONE",
+      methods = "NONE",
+      numbers = "NONE",
+      strings = "NONE",
+      comments = "italic",
+      keywords = "bold,italic",
+      constants = "NONE",
+      functions = "italic",
+      operators = "NONE",
+      variables = "NONE",
+      parameters = "NONE",
+      conditionals = "italic",
+      virtual_text = "italic",
+    },
+    options = {
+      transparency = true,
+    },
+    colorscheme = "onedark",
+  },
+}
